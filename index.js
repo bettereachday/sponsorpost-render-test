cd5ee01494342d021c63a2eb4272ba58db43e9c6
Jesper Bylund <mail@jesperbylund.com>
2021-10-09 11:01:32 +0200

    works locally now

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var request = require('request');
 
 app.get('/*', function (req, res) {
     var url = 'https://www.sponsorpost.co/host/test/sponsorpost.logo.png';
-    logger.info('/fileThumbnail going to url', url);
+    console.log('/fileThumbnail going to url', url);
     request.get(url).pipe(res);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
