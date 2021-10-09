c8205b1c4a06ce5effe6dd3bbde238b67a973ddd
Jesper Bylund <mail@jesperbylund.com>
2021-10-09 10:53:37 +0200

    add ignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Define Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+var request = require('request');
+
+app.get('/*', function (req, res) {
+    var url = 'https://www.sponsorpost.co/host/test/sponsorpost.logo.png';
+    logger.info('/fileThumbnail going to url', url);
+    request.get(url).pipe(res);
+});
